e719e8cabcbe5510e6c23d03d10f726e5d4766c8
Guilherme Devincenzi <gdevincenzi@gmail.com>
2020-04-27 19:47:39 -0300

    style: split one line functions into multiple lines

--- src/maze.clj
+++ src/maze.clj
@@ -17,7 +17,9 @@
 ;; Maze display
 ;;
 
-(defn cell-pos->maze-pos [[x y]] [(inc (* 2 x)) (inc (* 2 y))])
+(defn cell-pos->maze-pos
+  [[x y]]
+  [(inc (* 2 x)) (inc (* 2 y))])
 
 (defn cell->floor-tiles
   [[cell-pos connections]]
@@ -33,7 +35,11 @@
        (mapcat cell->floor-tiles)
        (into #{})))
 
-(defn wall-or-floor [floor-tiles coord] (if (contains? floor-tiles coord) "\033[1;34m. " "\033[1;32m# "))
+(defn wall-or-floor
+  [floor-tiles coord]
+  (if (contains? floor-tiles coord)
+    "\033[1;34m. "
+    "\033[1;32m# "))
 
 (defn generate-maze-coords
   [rows cols]
@@ -54,11 +60,17 @@
 ;; Maze generation
 ;;
 
-(defn valid-cell? [cells position] (some? (get cells position)))
+(defn valid-cell?
+  [cells position]
+  (some? (get cells position)))
 
-(defn valid-directions [cells position] (filter #(valid-cell? cells (val %)) (directions position)))
+(defn valid-directions
+  [cells position]
+  (filter #(valid-cell? cells (val %)) (directions position)))
 
-(defn unvisited? [cells position] (empty? (get cells position)))
+(defn unvisited?
+  [cells position]
+  (empty? (get cells position)))
 
 (defn maybe-get-unvisited-neighbors
   [cells position]
@@ -66,13 +78,20 @@
            (filter #(unvisited? cells (val %)))
            seq))
 
-(defn get-visited-cells [cells] (filter #(not-empty (val %)) cells))
+(defn get-visited-cells
+  [cells]
+  (filter #(not-empty (val %)) cells))
 
-(defn has-unvisited-neighbor? [cells position] (some? (maybe-get-unvisited-neighbors cells position)))
+(defn has-unvisited-neighbor?
+  [cells position]
+  (some? (maybe-get-unvisited-neighbors cells position)))
 
-(defn maybe-get-next-direction [cells position] (rand-nth (maybe-get-unvisited-neighbors cells position)))
+(defn maybe-get-next-direction
+  [cells position]
+  (rand-nth (maybe-get-unvisited-neighbors cells position)))
 
-(defn carve-path [position [direction next-position] cells]
+(defn carve-path
+  [position [direction next-position] cells]
   (-> cells
       (update position #(conj % direction))
       (update next-position #(conj % (inverse-direction direction)))))
